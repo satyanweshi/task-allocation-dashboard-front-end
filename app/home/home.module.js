0cc864d3174415eef509d23618dde91f85df50c1
Varun Sharma <varun.sharma2@xerox.com>
2016-09-29 10:46:51 +0530

    Changes for new menu as directive

--- app/home/home.module.js
+++ app/home/home.module.js
@@ -2,5 +2,6 @@
 
 // Define the `phoneDetail` module
 angular.module('home', [
-  'ngRoute'
+  'ngRoute',
+  'menuModule'
 ]);
---
+++ app/menu/menu.directive.js
@@ -0,0 +1,6 @@
+angular.module('menuModule')
+.directive('menu', function(){
+	return {
+		templateUrl : 'menu/menu.template.html'
+	};
+});
